cd387431aa29d200c2b52a8dfa8ba3025d4e87ff
Sgeary88 <97266345+Sgeary88@users.noreply.github.com>
2024-02-01 18:14:54 -0800

    got types to population on pokemon cards

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 const featuredContainer = document.querySelector(".featured-cards");
 
-let allPokemon = [];
-
-fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`)
-  .then((res) => res.json())
-  .then((data) => {
-    allPokemon = data.results;
-    console.log(allPokemon);
-    createCards(allPokemon);
-  });
-
 function getRandomIdNumber() {
   return Math.floor(Math.random() * 151 + 1);
 }
@@ -18,68 +8,62 @@ function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-// function createCards(data) {
-//   let pokeImg = data.sprites.other["official-artwork"].front_default;
-//   let name = data.name;
-//   let weight = data.weight;
-//   return (document.querySelector(".featured-cards").innerHTML = `
-// 	<div class="card">
-// 			<div class="card-image">
-// 				<img src="${pokeImg}" alt="${name}">
-// 			</div>
-// 			<div class="card-info">
-// 				<h3>${capitalizeFirstLetter(name)}</h3>
-// 				<p>${capitalizeFirstLetter(data.types[0].type.name)}</p>
-// 			</div>
-// 		</div>
-// 	`);
-// }
-
 async function getPokemon(pokeId) {
   try {
-    const [pokemon, pokemonSpecies] = await Promise.all([
-      fetch(`https://pokeapi.co.api/v2/pokemon/${pokeId}`).then((res) =>
-        res.json()
-      ),
-      fetch(`https://pokeapi.co.api/v2/pokemon-species/${pokeId}`).then((res) =>
+    const [pokemon] = await Promise.all([
+      fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`).then((res) =>
         res.json()
       ),
     ]);
-    return true;
+    return {
+      id: pokemon.id,
+      name: pokemon.name,
+      types: pokemon.types.map((type) => type.type.name),
+    };
   } catch (error) {
     console.error("Failed to fetch Pokemon");
   }
 }
 
-function createCards(pokemon) {
+async function fetchAndCreateFeaturedCards() {
+  try {
+    const pokemonData = await Promise.all(
+      Array.from({ length: 6 }, () => getPokemon(getRandomIdNumber()))
+    );
+
+    createCards(pokemonData);
+  } catch (error) {
+    console.error("Failed to fetch Pokemon data", error);
+  }
+}
+
+fetchAndCreateFeaturedCards();
+
+// Displays pokemon cards
+function createCards(pokemonData) {
   featuredContainer.innerHTML = "";
 
-  pokemon.slice(0, 6).forEach((pokemon) => {
-    const pokemonId = pokemon.url.split("/")[6];
+  pokemonData.forEach((pokemon) => {
     const cardItem = document.createElement("div");
 
     cardItem.className = "card";
     cardItem.innerHTML = `
-			<div class="card-image">
-				<img src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg" alt="${
-      pokemon.name
-    }"/>
+    <div class="card-image-wrapper">
+      <div class="card-image">
+				<img src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${
+          pokemon.id
+        }.svg" alt="${pokemon.name}"/>
+        </div>
 			</div>
 			<div class="card-info">
 			<h3>${capitalizeFirstLetter(pokemon.name)}</h3>
-			<p>${capitalizeFirstLetter(pokemonId.types[1].type.name)}</p>
+			<p>${capitalizeFirstLetter(pokemon.types[0])}</p>
 			</div>
-		`;
+    `;
 
     featuredContainer.appendChild(cardItem);
   });
 }
-
-// getPokemon(getRandomIdNumber());
-
-// -JS Framework- fetching data from api, display array, display at 3 values of the api(name, base xp, height)
-
-// - html has to be created programmatically.
 
 // - function to add favorites to a list
 
